1e95eae5ad7ad47d924649ee0c138dd696d81f6d
Shubham Khetan <janukhetan2005@gmail.com>
2024-04-01 10:53:05 +0530

    Cross made usable

--- flash_errors.py
+++ flash_errors.py
@@ -5,7 +5,7 @@ def user_beta_msti_nhi():
 def some_went_wrong():
     return flash(("error", "Something went wrong"))
 def login_success():
-    return flash(("success", "Loggen in successfully"))
+    return flash(("success", "Logged in successfully"))
 def acc_created():
     return flash(("success", "Account created successfully"))
 def trans_suc():
@@ -14,7 +14,7 @@ def trans_suc():
 def server_contact_error():
     return flash(("error", "Problem in conecting to the server"))
 def acc_al_pres():
-    return flash(("error", "Account already present at the server"))
+    return flash(("error", "Account already exists"))
 def wrng_pass():
     return flash(("error", "Either contact no. or passward is wrong"))
 def user_not_found():
